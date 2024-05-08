d0d1f1cb81072f0a50ecf5dab6a999a88e1d2016
Wong Teck Han <7806684+teckhan@users.noreply.github.com>
2024-05-08 13:47:29 +0800

    build: test github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ mode }) => ({
+  base: "/little-lives/",
   plugins: [vue()],
   resolve: {
     alias: {
